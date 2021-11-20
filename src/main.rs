4baea10826439be81d2df1ffadc9672fdb686148
Chris Kaltwasser <chris@chrisk.com>
2021-11-20 18:20:33 -0500

    using subcommand from examples

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,59 @@
-use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "RSHOT")]
-struct Opt {
-    /// Dump configuration and exit.
-    #[structopt(short = "g", long = "show-config")]
-    show_config: bool,
-    #[structopt(short = "f", long = "config-file")]
-    config_file: Option<String>,
+struct MakeCookie {
+    /// set supervising faerie <String>
+    #[structopt(name = "supervisor", default_value = "Puck", short = "s", long = "supervisor")]
+    supervising_faerie: String,
+    /// The faerie tree this cookie is being made in.
+    tree: Option<String>,
+    #[structopt(subcommand)] // Note that we mark a field as a subcommand
+    cmd: Command,
 }
 
-#[allow(dead_code)]
 #[derive(StructOpt, Debug)]
-#[structopt(name = "git", about = "the stupid content tracker")]
-enum Git {
-    #[structopt(name = "add")]
-    Add {
-        #[structopt(short = "i")]
-        interactive: bool,
-        #[structopt(short = "p")]
-        patch: bool,
-        #[structopt(parse(from_os_str))]
-        files: Vec<PathBuf>
+enum Command {
+    /// Pound acorns into flour for cookie dough.
+    Pound {
+        acorns: u32,
     },
-    #[structopt(name = "fetch")]
-    Fetch {
-        #[structopt(long = "dry-run")]
-        dry_run: bool,
-        #[structopt(long = "all")]
-        all: bool,
-        repository: Option<String>
+    /// Add magical sparkles -- the secret ingredient!
+    Sparkle {
+        /// magicality <number>
+        #[structopt(short, parse(from_occurrences))]
+        magicality: u64,
+        /// color <string>>
+        #[structopt(short)]
+        color: String,
     },
-    #[structopt(name = "commit")]
-    Commit {
-        #[structopt(short = "m")]
-        message: Option<String>,
-        #[structopt(short = "a")]
-        all: bool
+    Finish(Finish),
+}
+
+// Subcommand can also be externalized by using a 1-uple enum variant
+#[derive(StructOpt, Debug)]
+struct Finish {
+    /// <time> is expressed as an integer number
+    #[structopt(short)]
+    time: u32,
+    #[structopt(subcommand)] // Note that we mark a field as a subcommand
+    /// finish_type is a Glaze and a Powder
+    finish_type: FinishType,
+}
+
+// subsubcommand!
+#[derive(StructOpt, Debug)]
+enum FinishType {
+    Glaze {
+        /// <applications> is expressed as an integer number
+        applications: u32,
+    },
+    Powder {
+        flavor: String,
+        dips: u32,
     }
 }
 
-fn main () {
-    let opt = Opt::from_args();
-    println!("opt={:?}", opt);
-
-    let git = Git::from_args();
-    println!("git={:?}", git);
-
-    /*
-
-    match Git::from_args() {
-        Git::Add { interactive, patch, files } => {
-            println!("interactive={:?}", interactive);
-            println!("patch={:?}", patch);
-            println!("files={:?}", files);
-        },
-        Git::Commit { message, all } => {
-            println!("message={:?}", message);
-            println!("all={:?}", all);
-        }
-        _ => (),
-    }
-    */
+fn main() {
+    let command = Command::from_args();
+    println!("{:?}", command);
 }
-
